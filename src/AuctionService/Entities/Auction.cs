2b402fdd33d132f4fe21843ba074ac944674bb31
Felipe Nunes Work <felipe.rudra@hotmail.com>
2024-01-11 16:51:26 -0300

    add entidades

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,20 @@
+﻿namespace AuctionService.Entities
+{
+    public class Auction
+    {
+        public Guid Id { get; set; }
+        public int ReservePrice { get; set; } = 0;
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public int? SoldAmount { get; set; }
+        public int? CurrentHighBid { get; set; }
+        public DateTime CreateAT  { get; set; } = DateTime.UtcNow;
+        public DateTime UpdateAt { get; set; }= DateTime.UtcNow;
+        public DateTime AuctionEnd { get; set; }
+
+        public Status Status { get; set; }
+        public Item Item { get; set; }
+
+
+    }
+}
