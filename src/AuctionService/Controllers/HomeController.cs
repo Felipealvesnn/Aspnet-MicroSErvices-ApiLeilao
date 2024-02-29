b9b8a00989c9b5691eb3ba12992496b3de8088fc
Felipe Nunes Work <felipe.rudra@hotmail.com>
2024-02-29 11:41:31 -0300

    att

--- src/AuctionService/Controllers/HomeController.cs
+++ src/AuctionService/Controllers/HomeController.cs
@@ -55,6 +55,44 @@ namespace AuctionService.Controllers
             var auctionDto = _mapper.Map<AuctionDto>(auction);
             return CreatedAtAction(nameof(GetAuction), new { id = auction.Id }, auctionDto);
         }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await _auctionDb.Auctions.FirstOrDefaultAsync(a => a.Id == id);
+
+            if (auction == null) return NotFound();
+
+            //if (auction.Seller != User.Identity.Name) return Forbid();
+
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+
+            var result = await _auctionDb.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Error na hora de salvar");
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _auctionDb.Auctions.FirstOrDefaultAsync(a => a.Id == id);
+
+            if (auction == null) return NotFound();
+
+            _auctionDb.Remove(auction);
+
+            var result = await _auctionDb.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Error na hora de deletar");
+        }
+
 
 
     }
