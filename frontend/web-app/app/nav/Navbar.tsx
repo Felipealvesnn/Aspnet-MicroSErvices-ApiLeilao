1a50466297ee1a19871fdf24156f40651468b2f7
Felipe Nunes Work <felipe.rudra@hotmail.com>
2024-03-11 15:54:36 -0300

    att

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <header>
+            <div>
+                left
+            </div>
+            <div>
+                middle
+            </div>
+            <div>
+                Right
+            </div>
+
+        </header>
+    )
+}
---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h3 className="text-3xl
+      font-semibold
+      ">App leilao</h3>
+
+    </div>
+
+  );
+}
